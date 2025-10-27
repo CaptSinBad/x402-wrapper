4525bd3b04b295104166ce23a41185da25bc7297
CaptSinBad <97219716+CaptSinBad@users.noreply.github.com>
2025-10-27 03:09:57 +0100

    Add PrivyProvider to _app.tsx for authentication

---
+++ apps/dashboard/pages/_app.tsx
@@ -0,0 +1,17 @@
+// apps/dashboard/pages/_app.tsx
+import { PrivyProvider } from '@privy-io/react-auth';
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      config={{
+        loginMethods: ['wallet'],
+        appearance: { theme: 'light' },
+      }}
+    >
+      <Component {...pageProps} />
+    </PrivyProvider>
+  );
+}
